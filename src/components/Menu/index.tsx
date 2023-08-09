086b7ccfef4e3096f2306e75c340fdd032428995
RicardoHernandezVarela <rickvarela.dev@gmail.com>
2023-08-08 20:28:10 -0600

    menu responsive

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,15 +2,20 @@ import { Flex } from "@chakra-ui/react";
 import MenuOur from "../MenuOur";
 
 function Menu() {
-    return(
-     <Flex justifyContent="center" alignItems="center" gap="100px" my="70" >
-
-        <MenuOur precio= "100"/>
-        <MenuOur precio= "250"/>
-        <MenuOur precio= "400"/>
-
-     </Flex>
-    )
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      gap={{ base: "25px", xl: "80px" }}
+      my="70"
+      direction={{ base: "column", lg: "row" }}
+      px="15px"
+    >
+      <MenuOur precio="100" />
+      <MenuOur precio="250" />
+      <MenuOur precio="400" />
+    </Flex>
+  );
 }
 
 export default Menu;
