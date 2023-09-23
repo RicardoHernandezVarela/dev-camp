6ea6ab3ce947752d5f7aaeed29885b4f06976655
luisaDev14 <malu.23.dev@gmail.com>
2023-09-23 10:39:59 -0600

    Alumni page

---
+++ pages/alumni/index.tsx
@@ -0,0 +1,24 @@
+import { Flex } from "@chakra-ui/react";
+import PageWrapper from "@/src/components/Layout/PageWrapper";
+import SearchCard from "@/src/components/SearchCard";
+import AlumniCard from "@/src/components/AlumniCard";
+
+function Alumni() {
+    return(
+       <PageWrapper title={"Egresados"}>
+        <Flex direction="column">
+            <SearchCard/>
+
+            <Flex direction="column" pb="50px">
+                <AlumniCard/>
+                <AlumniCard/>
+                <AlumniCard/>
+                <AlumniCard/>
+            </Flex>
+        </Flex>
+
+       </PageWrapper> 
+    )
+}
+
+export default Alumni;
