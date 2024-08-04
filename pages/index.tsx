8c62ea36ac5e4ca2d9ef49653344ace8f1a21a32
RicardoHernandezVarela <rickvarela.dev@gmail.com>
2024-08-04 16:02:38 -0600

    removed data from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,46 @@ import OurTalents from "@/src/components/OurTalents";
 import PageWrapper from "@/src/components/Layout/PageWrapper";
 
 function Home() {
-  return <PageWrapper title={"Dev-camp"}></PageWrapper>;
+  return (
+    <PageWrapper title={"Dev-camp"}>
+      <Flex flexDirection="column">
+        <HomeHero
+          direction={"row"}
+          textColor={"#fff"}
+          bgColor={"main"}
+          buttonText={"Explore"}
+          buttonColor={"#000"}
+          buttonBg={"#fff"}
+        />
+
+        <TitleSection title={"dev-camp"} subtitle={"bienvenidos"} />
+
+        <StackList />
+
+        <HomeHero
+          direction={"row-reverse"}
+          textColor={"#212529"}
+          bgColor={"#fff"}
+          buttonText={"Learn More"}
+          buttonColor={"#fff"}
+          buttonBg={"#000"}
+        />
+
+        <HomeHero
+          direction={"row"}
+          textColor={"#212529"}
+          bgColor={"#fff"}
+          buttonText={"Learn More"}
+          buttonColor={"#fff"}
+          buttonBg={"#000"}
+        />
+
+        <TitleSection title={"Our Talents"} subtitle={"TEAM"} />
+
+        <OurTalents />
+      </Flex>
+    </PageWrapper>
+  );
 }
 
 export default Home;
